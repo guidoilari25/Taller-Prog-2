90be4a290ace898224fe6e31886ce48199982be8
LucioJ-R <luciojr2991@gmail.com>
2026-05-16 17:48:03 -0300

    Add files via upload

---
+++ src/Estadio.java
@@ -0,0 +1,13 @@
+public class Estadio {
+    private String nombre;
+    private int capacidad;
+
+    public String getNombre() {
+        return nombre;
+    }
+    public int getCapacidad() {
+        return capacidad;
+    
+    }
+    
+}
