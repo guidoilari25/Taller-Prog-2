962b46ec4333254a8f2585b59f3f137fc9c7afaa
LucioJ-R <luciojr2991@gmail.com>
2026-05-20 01:24:52 -0300

    Update Estadio.java

--- src/Partido/Estadio.java
+++ src/Partido/Estadio.java
@@ -4,12 +4,20 @@ public class Estadio {
     private String nombre;
     private int capacidad;
 
+    public Estadio() {
+        this.nombre = "";
+        this.capacidad = 0;
+    }
+    public Estadio(String nombre, int capacidad) [
+        this.nombre = nombre;
+        this.capacidad = capacidad;
+    }
+
     public String getNombre() {
         return nombre;
     }
     public int getCapacidad() {
         return capacidad;
-    
     }
     
 }
