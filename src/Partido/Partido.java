78ddf520d983d76e9714ff666f557d67b12ad266
LucioJ-R <luciojr2991@gmail.com>
2026-05-20 15:25:01 -0300

    Update Partido.java

--- src/Partido/Partido.java
+++ src/Partido/Partido.java
@@ -6,20 +6,44 @@ import java.sql.Time;
 public class Partido {
     private Date fecha;
     private Time horario;
-    private int duracion;
-    private int tiempoAdicional;
+    private float duracion;
+    private float tiempoAdicional;
+
+    public Partido() {
+        this.fecha = null;
+        this.horario = null;
+        this.duracion = 0;
+        this.tiempoAdicional = 0;
+    }
+    public Partido(Date fecha, Time horario, float duracion, float tiempoAdicional) {
+        this.fecha = fecha;
+        this.horario = horario;
+        this.duracion = duracion;
+        this.tiempoAdicional = tiempoAdicional;
+    }
 
     public Date getFecha() {
         return fecha;
     }
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
     public Time getHorario() {
         return horario;
     }
-    public int getDuracion() {
+    public void setHoarario(Time horario) {
+        this.horario = horario;
+    }
+    public float getDuracion() {
         return duracion;
     }
-    public int getTiempoAdicional() {
+    public void setDuracion(float duracion) {
+        this.duracion = duracion;
+    }
+    public float getTiempoAdicional() {
         return tiempoAdicional;
     }
-
+    public void setTiempoAdicional(float tiempoAdicional) {
+        this.tiempoAdicional = tiempoAdicional;
+    }
 }
