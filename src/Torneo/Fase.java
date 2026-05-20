228801049dac9bb024753e5b78cab979d583d15b
LucioJ-R <luciojr2991@gmail.com>
2026-05-20 15:49:02 -0300

    Update Fase.java

--- src/Torneo/Fase.java
+++ src/Torneo/Fase.java
@@ -3,8 +3,18 @@ package Torneo;
 public class Fase {
     private NombreFase nombre;
 
+    public Fase() {
+        this.fase = null;
+    }
+    public Fase(NombreFase fase) {
+        this.fase = fase;
+    }
+    
     public NombreFase getNombre() {
         return nombre;
+    }
+    public void setNombre(NombreFase fase) {
+        this.fase = fase;
     }
 
 }
