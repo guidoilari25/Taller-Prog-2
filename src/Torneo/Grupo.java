adae1b7ab0e08a19f27816f850fb6a84c96bc2b0
LucioJ-R <luciojr2991@gmail.com>
2026-05-20 17:14:56 -0300

    Update Grupo.java

--- src/Torneo/Grupo.java
+++ src/Torneo/Grupo.java
@@ -4,14 +4,29 @@ public class Grupo {
     private String identificacion;
     private String descripcion;
 
+    public Grupo() {
+        this.identificacion = "";
+        this.descripcion = "";
+    }
+    public Grupo(String identificacion, String descripcion) {
+        this.identificacion = identificacion;
+        this.descripcion = descripcion;
+    }
+
     public String getIndentificacion() {
         return identificacion;
+    }
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
     }
     public String getDescripcion() {
         return descripcion;
     }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
     public int obtenerPuntos(Seleccion s) {
         
     }
-
 }
