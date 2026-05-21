91d58d6db40fca59b5965f2a4ed7e3cbe057d18a
LucioJ-R <luciojr2991@gmail.com>
2026-05-20 22:19:41 -0300

    Update Mundial.java

--- src/Torneo/Mundial.java
+++ src/Torneo/Mundial.java
@@ -6,17 +6,38 @@ public class Mundial {
     private int fechaDesde;
     private int fechaHasta;
 
+    public Mundial() {
+        this(0, "", "", "");
+    }
+    public Mundial(int anno, String mascota, int fechaDesde, int fechaHasta) {
+        this.anno = anno;
+        this.mascota = mascota;
+        this.fechaDesde = fechaDesde;
+        this.fechaHasta = fechaHasta;
+    }
+
     public int getAnno() {
         return anno;
+    }
+    public void setAnno(int anno) {
+        this.anno = anno;
     }
     public String getMascota() {
         return mascota;
     }
+    public void setMascota(String mascota) {
+        this.mascota = mascota;
+    }
     public int getFechaDesde() {
         return fechaDesde;
+    }
+    public void setFechaDesde(int fechaDesde) {
+        this.fechaDesde = fechaDesde;
     }
     public int getFechaHasta() {
         return fechaHasta;
     }
-    
+    public void setFechaHasta(int fechaHasta) {
+        this.fechaHasta = fechaHasta;
+    }
 }
