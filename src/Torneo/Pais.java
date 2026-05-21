1df3174f8eb267707fd73190923806d85f67a262
LucioJ-R <luciojr2991@gmail.com>
2026-05-20 22:23:18 -0300

    Update Pais.java

--- src/Torneo/Pais.java
+++ src/Torneo/Pais.java
@@ -3,11 +3,25 @@ package Torneo;
 public class Pais {
     private String nombre;
     private String bandera;
+
+    public Pais() {
+        this("", "");
+    }
+    public Pais(String nombre, String bandera) {
+        this.nombre = nombre;
+        this.bandera = bandera;
+    }
     
     public String getNombre() {
         return nombre;
     }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
     public String getBandera() {
         return bandera;
+    }
+    public void setBandera(String bandera) {
+        this.bandera = bandera;
     }
 }
